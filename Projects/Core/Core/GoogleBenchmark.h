cb37cbb1303a4e8f2e6a995c14720d032fefbe30
Mani <>
2022-09-24 23:33:13 -0400

    Trying to fix the Linux github build.

--- Projects/Core/Core/GoogleBenchmark.h
+++ Projects/Core/Core/GoogleBenchmark.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#ifdef WIN32
 #include <codeanalysis\warnings.h>
 
 #pragma warning(push)
 #pragma warning(disable : ALL_CODE_ANALYSIS_WARNINGS)
 #include "benchmark/benchmark.h"
 #pragma warning(pop)
+#else
+#include "benchmark/benchmark.h"
+#endif

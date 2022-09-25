84a6cd6599fd0c06c0dbb2a6710dd334e3781d00
Mani <>
2022-09-24 23:33:13 -0400

    Trying to fix the Linux github build.

--- Projects/Core/Core/GoogleTest.h
+++ Projects/Core/Core/GoogleTest.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#ifdef WIN32
 #include <codeanalysis\warnings.h>
 
 #pragma warning(push)
 #pragma warning(disable : ALL_CODE_ANALYSIS_WARNINGS)
 #include "gtest/gtest.h"
 #pragma warning(pop)
+#else
+#include "gtest/gtest.h"
+#endif

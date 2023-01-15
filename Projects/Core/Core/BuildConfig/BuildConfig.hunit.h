afb3f6113a7211f67dbf6060bdd1da95c403099b
Mani <>
2023-01-14 23:51:51 -0500

    Added platform and compiler define for sanitizers.

--- Projects/Core/Core/BuildConfig/BuildConfig.hunit.h
+++ Projects/Core/Core/BuildConfig/BuildConfig.hunit.h
@@ -23,6 +23,36 @@ constexpr auto isCpp = true;
 constexpr auto isCpp = false;
 #endif
 
+#ifdef __clang__ 
+  #if defined(__has_feature) && __has_feature(address_sanitizer)
+    #define USING_ADDRESS_SANITISER 1
+  #endif
+
+  #if defined(__has_feature) && __has_feature(thread_sanitizer)
+    #define USING_THREAD_SANITISER 1
+  #endif
+
+#elif defined __GNUC__
+
+  #define USING_ADDRESS_SANITISER 0
+  #define USING_THREAD_SANITISER 0
+
+#elif defined _MSC_VER
+
+  #if defined(_PREFAST_) || defined(PVS_STUDIO)
+    #define BUILDCONFIG_CODE_ANALYSIS 1
+  #else
+    #define BUILDCONFIG_CODE_ANALYSIS 0
+  #endif
+
+  #if defined __SANITIZE_ADDRESS__ 
+    #define USING_ADDRESS_SANITISER 1
+  #endif
+
+  #define USING_THREAD_SANITISER 0
+#endif
+
+
 #if defined __GNUC__
   #define BUILDCONFIG_CDECL __attribute__((cdecl))
   #define BUILDCONFIG_STDCALL __attribute__((stdcall))
@@ -44,5 +74,15 @@ constexpr auto isCpp = false;
 #else
   #error Compiler not supported.
 #endif
+
+#if defined __GNUC__
+  #define PLATFORM_BREAK() (__asm__ volatile("int $0x03");)
+#elif (defined _MSC_VER)
+  #define PLATFORM_BREAK() (__nop(), __debugbreak())
+#else
+  #error Compiler not supported.
+#endif
+
+
 
 } // namespace BuildConfig

5a7f720bc931527efb5e4c1f929d6362df72b528
monamimani <monamimani@gmail.com>
2023-08-03 03:55:51 -0400

    TRy an other way of loading the vs env. So that also the tools like cmake get added.

---
+++ Scripts/Build/VsEnvironment.ps1
@@ -0,0 +1,39 @@
+
+function InvokeVcVarsAll
+{
+  param (
+  [Parameter()]
+  $Arch = 'x64',
+
+  [Parameter(ValueFromRemainingArguments=$true)]
+  $Arguments
+  )
+
+    $vsPath= GetVsInstallationPath
+    $vcvarsallPath = [IO.Path]::Combine($vsPath, "VC", "Auxiliary", "Build", "vcvarsall.bat")
+    
+    #write-output($vcvarsallPath)
+    
+    if($vcvarsallPath) {
+        cmd /s /c """$vcvarsallPath"" $Arch && set" | where { $_ -match '(\w+)=(.*)' } | foreach {
+            $null = new-item -force -path "Env:\$($Matches[1])" -value $Matches[2]
+        }
+    }
+
+    #Get-ChildItem env:
+}
+
+function LauncheVsDevShell
+{
+    $vsPath= GetVsInstallationPath
+    $vcvarsallPath = [IO.Path]::Combine($vsPath, "Common7", "Tools", "Launch-VsDevShell.ps1")
+}
+
+function GetVsInstallationPath
+{
+        Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted 
+    Install-Module VSSetup -Scope CurrentUser
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy UnTrusted 
+    $vsPath =Get-VSSetupInstance | Select-VSSetupInstance -Require 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64' -Latest
+    return $vsPath.InstallationPath
+}

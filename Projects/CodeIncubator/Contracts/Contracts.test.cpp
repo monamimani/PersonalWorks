cc60a328e44ce4d27240d71cbc5eb02b399432b0
monamimani <monamimani@gmail.com>
2024-06-03 04:18:02 -0400

    Fixing properly the build machine by removing the code it can't compile. Add beginning of contracts code.

---
+++ Projects/CodeIncubator/Contracts/Contracts.test.cpp
@@ -0,0 +1,15 @@
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+
+
+
+TEST_CASE("Contracts", "[Contracts]")
+{
+
+
+  SECTION("Success Path")
+  {
+    REQUIRE(true);
+  }
+}

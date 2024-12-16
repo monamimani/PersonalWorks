61c307c213332f1456e85c8a9366879514593bdd
monamimani <monamimani@gmail.com>
2024-12-15 19:36:11 -0500

    [CodeIncubator] Iteration on ContractsAssertions

--- Projects/CodeIncubator/main.test.cpp
+++ Projects/CodeIncubator/main.test.cpp
@@ -1,56 +1,12 @@
 
 #include <string>
-#include "TestUtilities/GoogleTest.h"
+#include "catch2/catch_session.hpp"
 
-// Google test sanitizer integration.
-extern "C" {
-void __ubsan_on_report() {
-  FAIL() << "Encountered an undefined behavior sanitizer error";
-}
-void __asan_on_error() {
-  FAIL() << "Encountered an address sanitizer error";
-}
-void __tsan_on_report() {
-  FAIL() << "Encountered a thread sanitizer error";
-}
-}  // extern "C"
 
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-  std::cout << "--- Google Test ---\n";
+  int result = Catch::Session().run( argc, argv );
 
-  auto cmdLine = std::vector<char*>(argv, argv + argc);
-  
-#ifdef _DEBUG
-  //std::string breakOnFaillure = "--gtest_break_on_failure";
-  //cmdLine.push_back(&breakOnFaillure[0]);
-  //
-  //std::string notCatchException = "--gtest_catch_exceptions=0";
-  //cmdLine.push_back(notCatchException.data());
-#endif // _DEBUG
-  cmdLine.emplace_back();
-
-  int argCount = static_cast<int>(cmdLine.size() - 1);
-  auto argsPtr = cmdLine.data();
-  ::testing::InitGoogleTest(&argCount, argsPtr);
-
-  // ::testing::GTEST_FLAG(filter) = "SpclMbFct*";
-  //::testing::GTEST_FLAG(filter) = "OpArity1DelegateLikeTestF*";
-
-  if (RUN_ALL_TESTS() == EXIT_FAILURE)
-  {
-    return EXIT_FAILURE;
-  }
-
-  //std::cout << "\n--- Google Benchmark ---\n";
-  //::benchmark::Initialize(&argc, argv);
-  //if (::benchmark::ReportUnrecognizedArguments(argc, argv))
-  //{
-  //  return EXIT_FAILURE;
-  //}
-  //::benchmark::RunSpecifiedBenchmarks();
-  //::benchmark::Shutdown();
-
-  return EXIT_SUCCESS;
+  return result;
 }

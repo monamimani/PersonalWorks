d807424ca8898493042acc539f93bf690dc82949
monamimani <492860+monamimani@users.noreply.github.com>
2025-08-17 03:41:42 -0400

    refactor(cmake): Organize CMake files into subdirectories
    This commit refactors the `CMake` directory by organizing the files into subdirectories based on their functionality. This improves the structure and maintainability of the build system.
    
    The new structure is as follows:
    - `CMake/BuildConfig`: Contains scripts for configuring the build process.
    - `CMake/Dependencies`: Contains scripts for managing dependencies.
    - `CMake/Targets`: Contains scripts for defining custom targets.
    - `CMake/Utilities`: Contains utility scripts.
    
    The paths in the `CMakeLists.txt` and other `.cmake` files have been updated to reflect the new structure.

---
+++ CMake/Targets/WarningsTarget.cmake
@@ -0,0 +1,5 @@
+add_library(PersonalWorks_warnings INTERFACE)
+add_library(PersonalWorks::PersonalWorks_warnings ALIAS PersonalWorks_warnings)
+
+include(CMake/BuildConfig/CompilerWarnings.cmake)
+add_target_interface_warnings(PersonalWorks_warnings OFF)

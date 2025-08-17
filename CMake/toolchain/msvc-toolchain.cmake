302b0ebc714ff9abe1b6595444f9250ffe7f4168
monamimani <492860+monamimani@users.noreply.github.com>
2025-08-17 02:34:05 -0400

    refactor(cmake): update toolchain files for vcpkg integration and compiler flags

--- CMake/toolchain/msvc-toolchain.cmake
+++ CMake/toolchain/msvc-toolchain.cmake
@@ -1,41 +1,54 @@
 # MSVC toolchain
+include_guard(GLOBAL)
+
+set(CMAKE_SYSTEM_NAME Windows CACHE STRING "")
 
 include(${CMAKE_CURRENT_LIST_DIR}/common-toolchain.cmake)
 
+set(CMAKE_CXX_COMPILER "cl")
+set(CMAKE_C_COMPILER "cl")
+
 # MSVC specific settings
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++latest /MP /utf-8 /diagnostics:caret")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP /utf-8 /diagnostics:caret")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP /utf-8 /diagnostics:caret")
 
+#I should set the dynamicdeoptimization in release mode. not in shipping builds
+#add_compile_options("/dynamicdeopt")
 
 if("$ENV{VSCMD_VER}" STREQUAL "")
   message(WARNING "Not in a developer environment, run cmake from a Visual Studio Developer terminal.")
 endif()
 
 set(MSVC_WARNINGS
-  /W4
-  /w14242
-  /w14254
-  /w14263
-  /w14265
-  /w14287
-  /we4289
-  /w14296
-  /w14311
-  /w14545
-  /w14546
-  /w14547
-  /w14549
-  /w14555
-  /w14619
-  /w14640
-  /w14826
-  /w14905
-  /w14906
-  /w14928
-  /permissive-
-  /WX
+   /W4 # Baseline reasonable warnings
+    /w14242 # 'identifier': conversion from 'type1' to 'type2', possible loss of data
+    /w14254 # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
+    /w14263 # 'function': member function does not override any base class virtual member function
+    /w14265 # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not
+
+    # be destructed correctly
+    /w14287 # 'operator': unsigned/negative constant mismatch
+    /we4289 # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside
+
+    # the for-loop scope
+    /w14296 # 'operator': expression is always 'boolean_value'
+    /w14311 # 'variable': pointer truncation from 'type1' to 'type2'
+    /w14545 # expression before comma evaluates to a function which is missing an argument list
+    /w14546 # function call before comma missing argument list
+    /w14547 # 'operator': operator before comma has no effect; expected operator with side-effect
+    /w14549 # 'operator': operator before comma has no effect; did you intend 'operator'?
+    /w14555 # expression has no effect; expected expression with side- effect
+    /w14619 # pragma warning: there is no warning number 'number'
+    /w14640 # Enable warning on thread un-safe static member initialization
+    /w14826 # Conversion from 'type1' to 'type2' is sign-extended. This may cause unexpected runtime behavior.
+    /w14905 # wide string literal cast to 'LPSTR'
+    /w14906 # string literal cast to 'LPWSTR'
+    /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+    /permissive- # standards conformance mode for MSVC compiler.
 )
 
 string(REPLACE ";" " " MSVC_WARNINGS_STR "${MSVC_WARNINGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MSVC_WARNINGS_STR}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MSVC_WARNINGS_STR}")
+
+include(${CMAKE_CURRENT_LIST_DIR}/vcpkg-toolchain.cmake)

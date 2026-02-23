e2e88d70819b9a1a98f878f7ac95a5c345f98195
Emmanuel Thivierge <492860+monamimani@users.noreply.github.com>
2026-02-23 03:58:15 -0500

    build(deps): update vulkan dependencies
    Update overlay ports for vulkan-headers and vulkan-loader, enabling C++ modules and updating vcpkg baseline.

--- Vcpkg/OverlayPorts/vulkan-loader-tmp/portfile.cmake
+++ Vcpkg/OverlayPorts/vulkan-loader-tmp/portfile.cmake
@@ -1,36 +1,33 @@
 
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-vcpkg_download_distfile(ARM_COMMENT_SYNTAX_FIX
-  URLS https://github.com/KhronosGroup/Vulkan-Loader/commit/ce3a4db90513c2dd10fbe56a21207945fbc9339f.patch?full_index=1
-  SHA512 e08d0be965f2430a5353a30339e1c29e5074d5b751f7894bdfec8c2031916b0e4d39fff1dad0e5ec94e4f2f296fac230dbfcb9a04af4b2d1ad51dffe270d232a
-  FILENAME vulkan-loader-arm-comment-syntax-ce3a4db90513c2dd10fbe56a21207945fbc9339f.patch
-)
-
-vcpkg_download_distfile(NINJA_CLANG_CL_WORKAROUND
-  URLS https://github.com/KhronosGroup/Vulkan-Loader/commit/4b043de5655d41cee12ef73d986cb7f7a7dbc239.patch?full_index=1
-  SHA512 327ce23dc1a4e68ef31a02e9847c7aa740f0a81cd62d3fecf5e7efd84149909f2377e031064346ecb6aa2c5ec094413627a3f4b34c674d8713fc1ca01f88fc22
-  FILENAME vulkan-loader-clang-cl-workaround-4b043de5655d41cee12ef73d986cb7f7a7dbc239.patch
-)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/Vulkan-Loader
-    REF "v1.4.304"
-    SHA512 7d4860820e2161934f93b20fa0403ba6a5e757dc4ef6012308f7ba504fe73d7b119d6516fa71b074108f2c633ec1f30812d9d0851da2f46485ba673f83b85c0b
+    REF "v1.4.331"
+    SHA512 cf582cc8cd243e3d4d9c1fdf79ee1bf99f86e10535968f929f2bedd669850362969fa50adf4f590aaa3ac39ab8e4340d95684048e499f6e8b7049c432e65ef45
     HEAD_REF main
-    #PATCHES
-    #    "${ARM_COMMENT_SYNTAX_FIX}"
-    #    "${NINJA_CLANG_CL_WORKAROUND}"
 )
 
 vcpkg_find_acquire_program(PYTHON3)
+# Needed to make port install vulkan.pc
+vcpkg_find_acquire_program(PKGCONFIG)
+set(ENV{PKG_CONFIG} "${PKGCONFIG}")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        xcb       BUILD_WSI_XCB_SUPPORT
+        xlib      BUILD_WSI_XLIB_SUPPORT
+        wayland   BUILD_WSI_WAYLAND_SUPPORT
+        directfb  BUILD_WSI_DIRECTFB_SUPPORT
+)
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
     -DBUILD_TESTS:BOOL=OFF
     -DPython3_EXECUTABLE=${PYTHON3}
+    ${FEATURE_OPTIONS}
 )
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
